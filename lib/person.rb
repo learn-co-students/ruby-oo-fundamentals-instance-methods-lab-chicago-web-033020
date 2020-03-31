2043f71cc368a11da99b3e8e106ae10589c68891
mack_mcquen <mackmcquen@comcast.net>
2020-03-31 18:16:20 -0500

    mack-mcquen-completed-lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,9 @@
 class Person
-    
     def talk
-       
-        puts "Hello World!"
-    
-    end
+       puts "Hello World!"
+     end
     
     def walk
-        
         puts "The Person is walking"
-    
     end
-
 end
